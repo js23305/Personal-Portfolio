90636bcb8a4a06457271a149cd2031fa918720db
js23305 <jasminesingla652@gmail.com>
2025-01-15 22:20:08 +0530

    Added another function called contacts

--- app.py
+++ app.py
@@ -12,6 +12,11 @@ def index():
 def about():
     return render_template('about.html')
 
+@app.route("/contact")
+
+def contact():
+        return render_template('contact.html')
+
 
 if __name__=='__main__':
     app.run(debug = True)
